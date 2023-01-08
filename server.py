001e2563995a4def341346a36d07833b4c5a34f8
arifberisha1 <93672583+arifberisha1@users.noreply.github.com>
2023-01-08 23:21:32 +0100

    Update server.py
    Final update

--- server.py
+++ server.py
@@ -1,35 +1,43 @@
-import Info
 import threading
 import socket
+import rsa_module
+import rsa
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((Info.host, Info.port))
+server.bind(('127.0.0.1', 5678))
 server.listen()
 clients = []
 aliases = []
+clients_public_keys = []
 leaving = []
+server_private_key = rsa_module.read_server_private_key()
 
 
 # Functions to handle the clients connection
 def broadcast(message):
     for client in clients:
-        client.send(message)
+        index = clients.index(client)
+        clients_public_key = clients_public_keys[index]
+        encrypted_message = rsa.encrypt(message.encode(), clients_public_key)
+        client.send(encrypted_message)
 
 
 def handle_client(client):
     while True:
         try:
             message = client.recv(1024)
-            msg = message.decode('utf-8')
-            print(msg)
-            broadcast(message)
+            decrypted_message = rsa.decrypt(message, server_private_key).decode()
+            print(decrypted_message)
+            broadcast(decrypted_message)
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             alias = aliases[index]
-            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
+            broadcast(f'{alias} has left the chat room!')
             aliases.remove(alias)
+            clients_public_key = clients_public_keys[index]
+            clients_public_keys.remove(clients_public_key)
             break
 
 
@@ -42,10 +50,14 @@ def receive():
         print(f'Connection is established with {str(address)}')
         client.send('alias?'.encode('utf-8'))
         alias = client.recv(1024)
+        ecpk = client.recv(1024)
+        dcpk = rsa.decrypt(ecpk, server_private_key).decode()
+        cpk = rsa.PublicKey.load_pkcs1(dcpk)
         aliases.append(alias)
         clients.append(client)
+        clients_public_keys.append(cpk)
         print(f'The alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        broadcast(f'{alias} has connected to the chat room')
         client.send('You are now connected!'.encode('utf-8'))
         thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
@@ -54,17 +66,12 @@ def receive():
 def send():
     msg = input("Message: ")
     message = f'Server: {msg}'
-    broadcast(message.encode('utf-8'))
+    broadcast(message)
 
 
 def list_clients():
     for alias in aliases:
         print(alias)
-
-
-def close_server():
-    broadcast("Stoping".encode('utf-8'))
-    raise Exception("Server closed!")
 
 
 def choices():
@@ -74,9 +81,11 @@ def choices():
             send()
         elif c == "2":
             list_clients()
+        elif c == "3":
+            broadcast("Stoping")
+            raise Exception("Server closed!")
         else:
-            close_server()
-            break
+            print("Choice must be a number between 1-3")
 
 
 if __name__ == "__main__":
